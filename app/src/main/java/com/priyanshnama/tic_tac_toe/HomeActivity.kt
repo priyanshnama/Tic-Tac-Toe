cf96ab19a3f63268f64ae8e9bebd1ad216e717ca
Phani-22 <phaniphani2212@gmail.com>
2021-11-15 17:24:43 +0530

    game screen ui designed

--- app/src/main/java/com/priyanshnama/tic_tac_toe/HomeActivity.kt
+++ app/src/main/java/com/priyanshnama/tic_tac_toe/HomeActivity.kt
@@ -2,10 +2,17 @@ package com.priyanshnama.tic_tac_toe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.priyanshnama.tic_tac_toe.databinding.ActivityHomeBinding
+import com.priyanshnama.tic_tac_toe.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
